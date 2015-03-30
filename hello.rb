2c48ab410044734683bff0f666ad8d497755dd42
Phoebe Hugh <phoebehugh@gmail.com>
2015-03-30 13:49:40 +0100

    Transfers files

--- hello.rb
+++ hello.rb
@@ -1,11 +1,5 @@
 require 'sinatra'
 
-get "/" do
-  "<div style='border: 3px dashed blue'>
-  <img src='http://bit.ly/1eze8aE'>
-  </div>"
-end
-
-get '/secret' do
-  'This is a secret page'
+get '/' do
+ erb :index
 end
